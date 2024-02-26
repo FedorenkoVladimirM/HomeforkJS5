a9feda1f16485130b036df36b6b1a7b525a67d4a
Vladimir Fedorenko <140750008+FedorenkoVladimirM@users.noreply.github.com>
2024-02-26 14:38:23 +0300

    commit1

---
+++ script.js
@@ -0,0 +1,21 @@
+function getMin (a,b) {
+    return a < b ? a:b;
+}
+
+
+function CheckParity (num) {
+    if (num % 2 == 0) {
+        return "Число четное";
+    }
+    else {
+        return "Число нечетное";
+    }
+}
+
+function PrintSquare(num) {
+    console.log (num*num);
+}
+
+function getSquare(num) {
+    return num*num;
+}
